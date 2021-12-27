3c57a413d264bf5ddd10fdbea7d4ed84d298fa3d
Demetrius Feijóo <demetrius.feijoo.91@gmail.com>
2021-12-27 20:17:25 -0300

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
+require("dotenv-flow").config();
+
 const server: express.Express = express();
 
-server.listen(5002, () => console.log("running"));
+server.listen(process.env.PORT, () =>
+  console.log(`running on port: ${process.env.PORT}`)
+);
 
 export default server;

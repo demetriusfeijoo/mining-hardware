1f42ff48bd2becdfa936e61aaf3717a36d88f16f
Demetrius Feijóo <demetrius.feijoo.91@gmail.com>
2021-12-28 19:08:17 -0300

    basic db modeling

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import server from "./server";
+import { PrismaClient } from "@prisma/client";
 
 server.listen(process.env.PORT, () =>
   console.log(`running on port: ${process.env.PORT}`)
 );
+
+const prisma = new PrismaClient();
